5037df3b26b7fd4bd7b493ed84daa9d53d3ef0e4
Long Nguyễn Bá <balongforwork@gmail.com>
2024-01-21 21:23:30 +0700

    Template product detail & form edit

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace BookStore.Areas.Admin.Controllers
 {
@@ -40,6 +41,74 @@ namespace BookStore.Areas.Admin.Controllers
             return View("ProductList", lstProduct);
         }
 
+        [Route("Edit/productId-{productId}")]
+        [HttpGet]
+        public IActionResult Edit(int productId)
+        {
+
+            var lstProduct = _context.Books
+                               .AsNoTracking()
+                               .Where(x => x.Isbn == productId)
+                               .Include(categories => categories.BookCategory)
+                               .Include(author => author.Author)
+                               .Include(publisher => publisher.Publisher)
+                               .OrderBy(x => x.Isbn)
+                               .FirstOrDefault();
+            return View(lstProduct);
+        }
+
+        [Route("Edit/productId-{productId}")]
+        [HttpPost]
+        public IActionResult Edit(int productId, IFormCollection values)
+        {
+            var bookToUpdate = _context.Books.FirstOrDefault(b => b.Isbn == productId);
+            if (bookToUpdate != null)
+            {
+                // Cập nhật các thuộc tính
+                //bookToUpdate.Isbn = values;
+                //bookToUpdate.AuthorId = updatedAuthorId;
+                //bookToUpdate.PublisherId = updatedPublisherId;
+                //bookToUpdate.BookCategoryId = updatedBookCategoryId;
+                //bookToUpdate.BookName = updatedBookName;
+                //bookToUpdate.Price = updatedPrice;
+                //bookToUpdate.PictureThumb = updatedPictureThumb;
+                //bookToUpdate.PublishYear = updatedPublishYear;
+                //bookToUpdate.Language = updatedLanguage;
+                //bookToUpdate.Status = updatedStatus;
+                //bookToUpdate.Discount = updatedDiscount;
+                //bookToUpdate.TotalPage = updatedTotalPage;
+                bookToUpdate.Description = values["Description"];
+                //bookToUpdate.Stock = updatedStock;
+
+                // Lưu thay đổi vào cơ sở dữ liệu
+                _context.SaveChanges();
+            }
+
+            //var lstProduct = _context.Books
+            //                   .AsNoTracking()
+            //                   .Where(x => x.Isbn == productId)
+            //                   .Include(categories => categories.BookCategory)
+            //                   .OrderBy(x => x.Isbn)
+            //                   .FirstOrDefault();
+            return View(bookToUpdate);
+        }
+
+
+        [Route("Detail/productId-{productId}")]
+        [HttpGet]
+        public IActionResult ProductDetail(int productId)
+        {
+
+            var product = _context.Books
+                               .AsNoTracking()
+                               .Where(x => x.Isbn == productId)
+                               .Include(categories => categories.BookCategory)
+                               .Include(author => author.Author)
+                               .Include(publisher => publisher.Publisher)
+                               .OrderBy(x => x.Isbn)
+                               .FirstOrDefault();
+            return View(product);
+        }
         // Add more actions as needed...
     }
 }
