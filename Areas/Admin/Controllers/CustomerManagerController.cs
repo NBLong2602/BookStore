6734c78b3578962e13a4939b8c582cc3887fc714
longorenda <balongforwork@gmail.com>
2024-01-29 20:26:17 +0700

    #deploy demo

---
+++ Areas/Admin/Controllers/CustomerManagerController.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Manager/Customer")]
+    public class CustomerManagerController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public CustomerManagerController(BookStoreContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [Route("")]
+        [Route("List")]
+        public IActionResult CustomerList()
+        {
+            var lstCustomer = _context.Customers
+                               .AsNoTracking()
+                               .Where(x => x.CustomerTypeId == 2)
+                               .OrderBy(x => x.Id)
+                               .ToList();
+            return View(lstCustomer);
+        }
+
+        [Route("Detail")]
+        [HttpGet]
+        public IActionResult CustomerDetail(int customerId)
+        {
+
+            var Customer = _context.Customers
+                               .AsNoTracking()
+                               .Where(x => x.Id == customerId)
+                               .FirstOrDefault();
+
+            return View(Customer);
+        }
+    }
+}
